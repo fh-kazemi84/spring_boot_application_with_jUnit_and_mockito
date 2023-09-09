ef52f242b33ab5868916379cd3ae1c47ddacdcbf
fh-kazemi84 <fh.kazemi84@gmail.com>
2023-09-09 15:10:41 +0200

    Create Project

---
+++ src/main/java/com/kazemi/spring_boot/_application_with_jUnit_and_mockito/SpringBootApplicationWithJUnitAndMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.kazemi.spring_boot._application_with_jUnit_and_mockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplicationWithJUnitAndMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplicationWithJUnitAndMockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kazemi/spring_boot/_application_with_jUnit_and_mockito/SpringBootApplicationWithJUnitAndMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kazemi.spring_boot._application_with_jUnit_and_mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApplicationWithJUnitAndMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
