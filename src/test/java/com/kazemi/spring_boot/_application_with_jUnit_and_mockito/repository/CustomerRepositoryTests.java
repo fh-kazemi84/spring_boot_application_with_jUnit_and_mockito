2c2686354a2d975c9a4ba75d27c74187b6aa2900
fh-kazemi84 <fh.kazemi84@gmail.com>
2023-09-10 22:33:17 +0200

    Update givenFirstNameAndLastName_whenFindByNativeSQLNamedParams_thenReturnCustomerObject() method.

--- src/test/java/com/kazemi/spring_boot/_application_with_jUnit_and_mockito/repository/CustomerRepositoryTests.java
+++ src/test/java/com/kazemi/spring_boot/_application_with_jUnit_and_mockito/repository/CustomerRepositoryTests.java
@@ -20,7 +20,7 @@ import java.util.Optional;
 @RunWith(SpringRunner.class)
 @DataJpaTest
 @AutoConfigureTestDatabase(replace=AutoConfigureTestDatabase.Replace.NONE)
-public class CustomerRepositoryTest {
+public class CustomerRepositoryTests {
 
     @Autowired
     private CustomerRepository customerRepository;
