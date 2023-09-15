69bec0e56f03fb3fd13749fc8003dfcd09557a12
fh-kazemi84 <fh.kazemi84@gmail.com>
2023-09-15 22:14:32 +0200

    Add getCustomerById method.

--- src/main/java/com/kazemi/spring_boot/_application_with_jUnit_and_mockito/controller/CustomerController.java
+++ src/main/java/com/kazemi/spring_boot/_application_with_jUnit_and_mockito/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.kazemi.spring_boot._application_with_jUnit_and_mockito.controller;
 import com.kazemi.spring_boot._application_with_jUnit_and_mockito.model.Customer;
 import com.kazemi.spring_boot._application_with_jUnit_and_mockito.service.CustomerService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
@@ -32,4 +33,13 @@ public class CustomerController {
     public List<Customer> getAllCustomers(){
         return customerService.getAllCustomers();
     }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable("id") long customerId){
+        return customerService.getCustomerById(customerId)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+
 }
