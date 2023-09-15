fb37b8bbb4f775493a72e2af076eefaff60d7a19
fh-kazemi84 <fh.kazemi84@gmail.com>
2023-09-15 23:24:27 +0200

    Add test createdCustomer.

--- src/test/java/com/kazemi/spring_boot/_application_with_jUnit_and_mockito/controller/CustomerControllerTests.java
+++ src/test/java/com/kazemi/spring_boot/_application_with_jUnit_and_mockito/controller/CustomerControllerTests.java
@@ -4,11 +4,24 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kazemi.spring_boot._application_with_jUnit_and_mockito.model.Customer;
 import com.kazemi.spring_boot._application_with_jUnit_and_mockito.service.CustomerService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.BDDMockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 /**
  * @author fh.kazemi
@@ -35,5 +48,28 @@ public class CustomerControllerTests {
                 .lastName("Kazemi")
                 .email("fh.kazemi84@gmail.com")
                 .build();
+    }
+
+    @Test
+    public void givenCustomerObject_whenCreateCustomer_thenReturnSavedCustomer() throws Exception{
+
+        // given - precondition or setup
+        given(customerService.saveCustomer(any(Customer.class)))
+                .willAnswer((invocation)-> invocation.getArgument(0));
+
+        // when - action or behaviour that we are going test
+        ResultActions response = mockMvc.perform(post("/api/customers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(customer)));
+
+        // then - verify the result or output using assert statements
+        response.andDo(print()).
+                andExpect(status().isCreated())
+                .andExpect(jsonPath("$.firstName",
+                        is(customer.getFirstName())))
+                .andExpect(jsonPath("$.lastName",
+                        is(customer.getLastName())))
+                .andExpect(jsonPath("$.email",
+                        is(customer.getEmail())));
     }
 }
