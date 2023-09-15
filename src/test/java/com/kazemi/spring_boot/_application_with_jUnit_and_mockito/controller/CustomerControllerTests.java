db62700f906f12dfe7c5d153bbb67e18acd42753
fh-kazemi84 <fh.kazemi84@gmail.com>
2023-09-15 23:54:11 +0200

    Add test updatedCustomer when get 404 error.

--- src/test/java/com/kazemi/spring_boot/_application_with_jUnit_and_mockito/controller/CustomerControllerTests.java
+++ src/test/java/com/kazemi/spring_boot/_application_with_jUnit_and_mockito/controller/CustomerControllerTests.java
@@ -152,4 +152,27 @@ public class CustomerControllerTests {
                 .andExpect(jsonPath("$.lastName", is(updatedCustomer.getLastName())))
                 .andExpect(jsonPath("$.email", is(updatedCustomer.getEmail())));
     }
+
+    // JUnit test for update customer REST API - negative scenario
+    @Test
+    public void givenUpdatedCustomer_whenUpdateCustomer_thenReturn404() throws Exception{
+        // given - precondition or setup
+        Customer updatedCustomer = Customer.builder()
+                .firstName("Fati")
+                .lastName("Ka")
+                .email("kazemi@gmail.com")
+                .build();
+        given(customerService.getCustomerById(customer.getId())).willReturn(Optional.empty());
+        given(customerService.updateCustomer(any(Customer.class)))
+                .willAnswer((invocation)-> invocation.getArgument(0));
+
+        // when -  action or the behaviour that we are going test
+        ResultActions response = mockMvc.perform(put("/api/customers/{id}", customer.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(updatedCustomer)));
+
+        // then - verify the output
+        response.andExpect(status().isNotFound())
+                .andDo(print());
+    }
 }
