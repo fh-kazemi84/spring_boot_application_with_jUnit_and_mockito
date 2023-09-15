9d25f14cd9696752355fabf0a52f3a84f5f3e209
fh-kazemi84 <fh.kazemi84@gmail.com>
2023-09-15 23:20:39 +0200

    Create setup method.

--- src/test/java/com/kazemi/spring_boot/_application_with_jUnit_and_mockito/controller/CustomerControllerTests.java
+++ src/test/java/com/kazemi/spring_boot/_application_with_jUnit_and_mockito/controller/CustomerControllerTests.java
@@ -1,11 +1,14 @@
 package com.kazemi.spring_boot._application_with_jUnit_and_mockito.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kazemi.spring_boot._application_with_jUnit_and_mockito.model.Customer;
 import com.kazemi.spring_boot._application_with_jUnit_and_mockito.service.CustomerService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.BeforeEach;
+
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
 
 /**
  * @author fh.kazemi
@@ -19,5 +22,18 @@ public class CustomerControllerTests {
     @MockBean
     private CustomerService customerService;
 
+
     private ObjectMapper objectMapper;
+
+    private Customer customer;
+
+    @BeforeEach
+    public void setup(){
+        customer = Customer.builder()
+                .id(1L)
+                .firstName("Fatemeh")
+                .lastName("Kazemi")
+                .email("fh.kazemi84@gmail.com")
+                .build();
+    }
 }
