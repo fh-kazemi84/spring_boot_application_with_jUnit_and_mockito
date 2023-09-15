25c8ee4e3d1e18bedba5376cc948b2128446edf7
fh-kazemi84 <fh.kazemi84@gmail.com>
2023-09-15 22:11:34 +0200

    Add createCustomer method.

--- src/main/java/com/kazemi/spring_boot/_application_with_jUnit_and_mockito/controller/CustomerController.java
+++ src/main/java/com/kazemi/spring_boot/_application_with_jUnit_and_mockito/controller/CustomerController.java
@@ -1,9 +1,10 @@
 package com.kazemi.spring_boot._application_with_jUnit_and_mockito.controller;
 
+import com.kazemi.spring_boot._application_with_jUnit_and_mockito.model.Customer;
 import com.kazemi.spring_boot._application_with_jUnit_and_mockito.service.CustomerService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author fh.kazemi
@@ -17,5 +18,11 @@ public class CustomerController {
 
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Customer createCustomer(@RequestBody Customer customer){
+        return customerService.saveCustomer(customer);
     }
 }
