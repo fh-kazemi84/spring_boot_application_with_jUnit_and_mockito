73482dfa9e92c779e01aabb78f746d14af471be8
fh-kazemi84 <fh.kazemi84@gmail.com>
2023-09-17 12:37:21 +0200

    Add test createCustomer.

--- src/test/java/com/kazemi/spring_boot/_application_with_jUnit_and_mockito/Integration/CustomerControllerIntegrationTests.java
+++ src/test/java/com/kazemi/spring_boot/_application_with_jUnit_and_mockito/Integration/CustomerControllerIntegrationTests.java
@@ -1,12 +1,22 @@
 package com.kazemi.spring_boot._application_with_jUnit_and_mockito.Integration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kazemi.spring_boot._application_with_jUnit_and_mockito.model.Customer;
 import com.kazemi.spring_boot._application_with_jUnit_and_mockito.repository.CustomerRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * @author fh.kazemi
@@ -23,10 +33,35 @@ public class CustomerControllerIntegrationTests {
 
     @Autowired
     private ObjectMapper objectMapper;
+    private Customer customer;
 
     @BeforeEach
     void setup(){
         customerRepository.deleteAll();
+        customer = Customer.builder()
+                .id(1L)
+                .firstName("Fatemeh")
+                .lastName("Kazemi")
+                .email("fh.kazemi84@gmail.com")
+                .build();
     }
 
+    @Test
+    public void givenCustomerObject_whenCreateCustomer_thenReturnSavedCustomer() throws Exception{
+
+        // when - action or behaviour that we are going test
+        ResultActions response = mockMvc.perform(post("/api/customers")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(customer)));
+
+        // then - verify the result or output using assert statements
+        response.andDo(print()).
+                andExpect(status().isCreated())
+                .andExpect(jsonPath("$.firstName",
+                        is(customer.getFirstName())))
+                .andExpect(jsonPath("$.lastName",
+                        is(customer.getLastName())))
+                .andExpect(jsonPath("$.email",
+                        is(customer.getEmail())));
+    }
 }
